78d75d130b88750c5cfc462e66919e03794a4285
noah-mclain <123869276+noah-mclain@users.noreply.github.com>
2024-01-29 21:35:15 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        // Even_or_Odd numb = new Even_or_Odd();
+        // numb.main();
+
+        // Factorial num = new Factorial();
+        // num.main();
+
+        // Fibb num = new Fibb();
+        // num.main();
+
+        // Palindrome str = new Palindrome();
+        // str.main();
+
+        // Prime_Num num = new Prime_Num();
+        // num.main();
+
+        // Grade_Calc m1 = new Grade_Calc();
+        // m1.main();
+
+        // Array_Manip arr = new Array_Manip();
+        // arr.main();
+
+        Pass_Gen pass = new Pass_Gen();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(pass.getPass(18));
+        }
+
+        
+
+    }
+    
+}
+
---
+++ Pass_Gen.java
@@ -0,0 +1,38 @@
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class Pass_Gen {
+
+    final char[] SYMBOLS = "!@#$%^&*".toCharArray();
+    final char[] LOWERCASE = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    final char[] UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    final char[] NUMBERS = "0123456789".toCharArray();
+    final char[] ALL_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789^$*.[]{}()?-\"!@#%&/\\,><':;|_~`".toCharArray();
+    final Random rand = new SecureRandom();
+
+    public String getPass(int length) {
+
+        assert length >= 4;
+        char[] password = new char[length];
+
+        //get the requirements out of the way
+        password[0] = LOWERCASE[rand.nextInt(LOWERCASE.length)];
+        password[1] = UPPERCASE[rand.nextInt(UPPERCASE.length)];
+        password[2] = NUMBERS[rand.nextInt(NUMBERS.length)];
+        password[3] = SYMBOLS[rand.nextInt(SYMBOLS.length)];
+
+        //populate rest of the password with random chars
+        for (int i = 4; i < length; i++) {
+            password[i] = ALL_CHARS[rand.nextInt(ALL_CHARS.length)];
+        }
+
+        //shuffle it up
+        for (int i = 0; i < password.length; i++) {
+            int randomPosition = rand.nextInt(password.length);
+            char temp = password[i];
+            password[i] = password[randomPosition];
+            password[randomPosition] = temp;
+        }
+        return new String(password);
+    }
+}
